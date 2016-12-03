f7309ba4005d657e8173de5b8629989a5a0c75c4
vmysak <moandorchik@gmail.com>
2016-12-03 16:10:58 +0200

    Update resizer-install.sh

--- resizer-install.sh
+++ resizer-install.sh
@@ -4,3 +4,4 @@ yum install python-pip ImageMagick python-magic
 pip install --allow-all-external Wand configparser optparse_lite
 cp ./resizer.py /usr/bin/resizer
 chmod +x /usr/bin/resizer
+cp ./imageresizer.desktop /usr/share/kservices5/ServiceMenus/
