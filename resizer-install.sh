1736a0a274220de05efb9db56ffaf143ee6cadb7
vmysak <moandorchik@gmail.com>
2017-10-19 00:08:45 +0300

    Update resizer-install.sh

--- resizer-install.sh
+++ resizer-install.sh
@@ -5,3 +5,4 @@ pip install --allow-all-external Wand configparser optparse_lite
 cp -rf ./resizer.py /usr/bin/resizer
 chmod +x /usr/bin/resizer
 cp -rf ./imageresizer.desktop /usr/share/kservices5/ServiceMenus/
+kbuildsycoca5
