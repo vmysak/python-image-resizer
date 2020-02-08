e47f158451695366ca557b81207868c16494f5be
vmysak <moandorchik@gmail.com>
2020-02-08 12:39:58 +0200

    Update resizer-install.sh

--- resizer-install.sh
+++ resizer-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install python-pip ImageMagick python-magic
+yum install python-pip ImageMagick python3-magic
 pip install --allow-all-external Wand configparser optparse_lite
 cp -rf ./resizer.py /usr/bin/resizer
 chmod +x /usr/bin/resizer
